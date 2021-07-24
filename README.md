4dbf63718fa3fded463678b5654897225e344e5e
jedi <amajidsinar@gmail.com>
2021-07-24 20:08:56 +0700

    initialize contract

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Multisig Wallet
-
 Multisig wallet is a wallet with multiple owners, whereas in order for a transaction to occur, m owner must authorize the transaction, with m <= n. If the condition is not met, transaction is reverted and would not occur.
---
+++ contracts/Multisig.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+contract MultiSigWallet {
+    function submitTransaction() {};
+
+    function confirmTransaction() {};
+
+    function executeTransaction() {};
+
+    function revertTransaction() {};
+}
