52b9aeb537782592e0e6a1dab7c2e284768c2f06
jedi <amajidsinar@gmail.com>
2021-07-26 22:05:55 +0700

    adding constructor

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.7.6;
 
 contract MultiSigWallet {
-    function submitTransaction() {};
 
-    function confirmTransaction() {};
+    mapping(address => uint) public owners;
 
-    function executeTransaction() {};
+    // Init constructor
+    constructor (address[] _owners, uint _num_validators) {
+        require(_num_validators > 0, 'number of validators must be greater than 0');
+        require(_num_validators <= len(_owners));
+        (for uint i = 0; i < len(_owners); i++) {
+            owners[i] = _owners;
+        }
+    }
+    
+    // function submitTransaction() {};
 
-    function revertTransaction() {};
+    // function confirmTransaction() {};
+
+    // function executeTransaction() {};
+
+    // function revertTransaction() {};
 }
