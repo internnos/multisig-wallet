5d9a4c74af25ce9c8a1dd346527c750537920bc5
jedi <amajidsinar@gmail.com>
2021-07-26 22:36:00 +0700

    add constructor

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-contract MultiSigWallet {
+contract MultiSigWallet{
 
     mapping(address => uint) public owners;
+    uint public num_validators;
 
     // Init constructor
-    constructor (address[] _owners, uint _num_validators) {
+    constructor(address[] memory _owners, uint _num_validators) {
         require(_num_validators > 0, 'number of validators must be greater than 0');
-        require(_num_validators <= len(_owners));
-        (for uint i = 0; i < len(_owners); i++) {
-            owners[i] = _owners;
+        require(_num_validators <= _owners.length);
+        for (uint i = 0; i < _owners.length; i++) {
+            address _owner = _owners[i];
+            owners[_owner] = i;
+            num_validators = i;
         }
+        
     }
     
     // function submitTransaction() {};
