408537712abc23a289c68890db8713bedc2933f6
jedi <amajidsinar@gmail.com>
2021-07-28 18:16:10 +0700

    add positive cases

--- tests/test_submit_transaction.py
+++ tests/test_submit_transaction.py
@@ -25,3 +25,23 @@ def test_trx_to_one_of_owner_2():
     with pytest.raises(VirtualMachineError) as excinfo:
         multisig_wallet.submitTransaction(trx_to, trx_amount)    
     assert str(excinfo.value)=="revert: cannot send ether to one of the multisig owner"
+
+def test_trx_to_other_account_1():
+    address_smart_contract = accounts[0]
+    address_eoa_owners = accounts[1:3]
+    trx_to = accounts[5]
+    trx_amount = "1000 wei"
+
+    multisig_wallet = MultiSigWallet.deploy(address_eoa_owners, 2, {'from': address_smart_contract})
+    multisig_wallet.submitTransaction(trx_to, trx_amount)
+
+
+def test_trx_to_other_account_2():
+    address_smart_contract = accounts[0]
+    address_eoa_owners = accounts[1:3]
+    trx_to = accounts[10]
+    trx_amount = "1000 wei"
+
+    multisig_wallet = MultiSigWallet.deploy(address_eoa_owners, 2, {'from': address_smart_contract})
+    multisig_wallet.submitTransaction(trx_to, trx_amount)    
+    
